27dc95f9a14ee3344fec843acd838aae9c0ab494
Ümit Yılmaz <42136540+umutyilmaz44@users.noreply.github.com>
2020-10-22 00:37:00 +0300

    Delete JWTSettings.cs

--- base-app-webapi-old/Helper/JWTSettings.cs
+++
@@ -1,9 +0,0 @@
-namespace base_app_webapi.Models
-{
-    public class JWTSettings
-    {
-        public string SecretKey { get; set; }
-        public string Issuer { get; set; }
-        public string Audience { get; set; }
-    }
-}
